9060a3b3ad8dc5dc0e5f6ed68fb819bdf43d78fb
stelemme <stef.lemmens@ugent.be>
2022-06-08 23:53:23 +0200

    expand database

---
+++ formatChanger.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import os
+
+path_dir = "C:/Users/Stef/Documents/test/gifs"
+
+for filename in os.listdir(path_dir):
+  f = os.path.join(path_dir, filename)
+  if f[-6:-4] == "-s":
+    img = Image.open(f)
+    img.save(f'{f[38:-6]}.png')
