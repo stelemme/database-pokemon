6c5e3267b3176fc200f2673420116ad6221320c8
stelemme <stef-lemmens@hotmail.com>
2023-09-27 12:24:08 +0200

    expand database

--- canvasChanger.py
+++ canvasChanger.py
@@ -94,4 +94,4 @@ original_width, original_height = check_max_size(input_path)
 max_size = max(int(original_height), int(original_width))
 
 #change_canvas(input_path, output_path, max_size)
-change_canvas_animated(input_path, output_path, max_size)
+change_canvas_animated(input_path, output_path, 96)
