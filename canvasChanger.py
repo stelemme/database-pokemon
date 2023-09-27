e122b3c3f64e9ac3709320734c1a3a0d9f2fabbe
stelemme <stef-lemmens@hotmail.com>
2023-09-27 12:19:25 +0200

    expand database

--- canvasChanger.py
+++ canvasChanger.py
@@ -1,24 +1,63 @@
-from PIL import Image
+from PIL import Image, ImageSequence
 import os
 
-def change_canvas(input_dir_path, output_dir_path):
+def change_canvas(input_dir_path, output_dir_path, max_size=56):
     for filename in os.listdir(input_dir_path):
         f = os.path.join(input_dir_path, filename)
 
         image = Image.open(f)
 
-        original_width, original_height = image.size
-        #max_size = max(original_height, original_width)
-        max_size = 56
-
         new_image = Image.new("RGBA", (max_size, max_size), (0,0,0,0))
 
         offset_x = (max_size - original_height) // 2
-        offset_y = (max_size - original_height)
+        offset_y = (max_size - original_height) // 2
 
         new_image.paste(image, (offset_x, offset_y))
 
         new_image.save(f'{output_dir_path}{filename}')
+
+def change_canvas_animated(input_dir_path, output_dir_path, max_size=56):
+    for filename in os.listdir(input_dir_path):
+        f = os.path.join(input_dir_path, filename)
+
+        image = Image.open(f)
+
+        # Check if the image is animated (has multiple frames)
+        if hasattr(image, 'n_frames') and image.n_frames > 1:
+            frames = []
+            durations = []
+
+            for frame in ImageSequence.Iterator(image):
+                original_width, original_height = frame.size
+
+                new_frame = Image.new("RGBA", (max_size, max_size), (0, 0, 0, 0))
+
+                offset_x = (max_size - original_width) // 2
+                offset_y = (max_size - original_height) // 2
+
+                new_frame.paste(frame, (offset_x, offset_y))
+
+                frames.append(new_frame)
+                durations.append(frame.info.get('duration', 100))  # Get frame duration
+
+            frames[0].save(
+                f'{output_dir_path}/{filename}',
+                save_all=True,
+                append_images=frames[1:],
+                duration=durations,  # Set frame durations
+                loop=image.info.get('loop', 0)  # Set loop count
+            )
+        else:
+            new_image = Image.new("RGBA", (max_size, max_size), (0, 0, 0, 0))
+
+            original_width, original_height = image.size
+
+            offset_x = (max_size - original_width) // 2
+            offset_y = (max_size - original_height) // 2
+
+            new_image.paste(image, (offset_x, offset_y))
+
+            new_image.save(f'{output_dir_path}/{filename}')
 
 def check_max_size(input_dir_path):
     max_width = 0
@@ -46,8 +85,13 @@ def check_max_size(input_dir_path):
     print(f'The Max Height is: {max_height} pixels')
     print(height_img_name)
 
+    return max_width, max_height
+
 input_path = "_input/"
 output_path = "_output/"
-""" check_max_size(input_path) """
 
-change_canvas(input_path, output_path)
+original_width, original_height = check_max_size(input_path)
+max_size = max(int(original_height), int(original_width))
+
+#change_canvas(input_path, output_path, max_size)
+change_canvas_animated(input_path, output_path, max_size)
